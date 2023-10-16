46c53dfcf16e99d864a7a16b39c768a35572ef7f
Alex H <ahugli@uw.edu>
2023-10-16 02:59:08 -0700

    V1.0.0

---
+++ README.md
@@ -0,0 +1,95 @@
+# Spellcast Solver
+
+A Python script to solve the Discord game Spellcast easily. The script generates the highest-scoring word from a given grid of letters, considering a special character index for bonus points.
+
+## Table of Contents
+
+1. [Getting Started](#getting-started)
+2. [Prerequisites](#prerequisites)
+3. [Installation](#installation)
+   - [Windows](#windows)
+   - [macOS](#macos)
+   - [Linux](#linux)
+4. [Usage](#usage)
+5. [Contributing](#contributing)
+6. [License](#license)
+7. [Contact](#contact)
+
+## Getting Started
+
+These instructions will help you set up and use the Spellcast Solver on your local machine for development and testing purposes.
+
+### Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- You have installed Python 3.x.
+- You have a Windows, macOS, or Linux machine.
+
+### Installation
+
+#### Windows
+
+1. Download the Python code file from the repository.
+2. Open a command prompt and navigate to the directory containing the code file.
+3. Install any required dependencies using `pip`:
+
+```bash
+pip install -r requirements.txt
+```
+
+#### macOS
+
+1. Download the Python code file from the repository.
+2. Open a terminal and navigate to the directory containing the code file.
+3. Install any required dependencies using `pip`:
+
+```bash
+pip install -r requirements.txt
+```
+
+#### Linux
+
+1. Download the Python code file from the repository.
+2. Open a terminal and navigate to the directory containing the code file.
+3. Install any required dependencies using `pip`:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+After installing the required dependencies, you can use the Spellcast Solver as follows:
+
+```bash
+python spellcast_solver.py
+```
+
+The script will prompt you to enter the grid of letters and the special character index. It will then generate the highest-scoring word based on the given input.
+
+## Contributing
+
+If you want to contribute to the project, please follow these steps:
+
+1. Fork the repository.
+2. Create a new branch: `git checkout -b <branch_name>`.
+3. Make your changes and commit them: `git commit -m '<commit_message>'`.
+4. Push your changes to the branch: `git push origin <branch_name>`.
+5. Create a pull request.
+
+Please adhere to the project's coding style and conventions, and provide clear, concise commit messages.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE.md). Please read the license file for more information.
+
+## Contact
+
+If you have any questions or need further assistance, please feel free to reach out to the project maintainer:
+
+- Name: Alex Hugli
+- Email: alexanderhugli@hotmail.com
+- GitHub: [@alexfacehead](https://github.com/alexfacehead)
+
+Please include the project title (Spellcast Solver) in the subject line of your email for a faster response.
---
+++ spellcast.py
@@ -0,0 +1,173 @@
+from english_words import get_english_words_set
+
+# Constants
+GRID_SIZE = 5
+LETTER_VALUES = {
+    'a': 1, 'b': 4, 'c': 5, 'd': 3,
+    'e': 1, 'f': 5, 'g': 3, 'h': 4,
+    'i': 1, 'j': 7, 'k': 6, 'l': 3,
+    'm': 4, 'n': 2, 'o': 1, 'p': 4,
+    'q': 10, 'r': 2, 's': 2, 't': 3,
+    'u': 4, 'v': 5, 'w': 5, 'x': 7,
+    'y': 4, 'z': 8
+}
+
+# Load dictionary words
+web2lowerset = get_english_words_set(['web2'], lower=True)
+
+class TrieNode:
+    """A node in the Trie data structure."""
+
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
+class Trie:
+    """A Trie data structure for efficient word search and insertion."""
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        """Insert a word into the Trie."""
+        node = self.root
+        for letter in word:
+            if letter not in node.children:
+                node.children[letter] = TrieNode()
+            node = node.children[letter]
+        node.is_word = True
+
+    def remove(self, word):
+        """Remove a word from the Trie."""
+
+        def _remove(node, idx):
+            if idx == len(word):
+                if node.is_word:
+                    node.is_word = False
+                    return len(node.children) == 0
+                return False
+
+            letter = word[idx]
+            if letter not in node.children or not _remove(node.children[letter], idx + 1):
+                return False
+
+            if len(node.children[letter].children) == 0:
+                del node.children[letter]
+
+            return len(node.children) == 0
+
+        _remove(self.root, 0)
+
+    def search(self, word):
+        """Search for a word in the Trie."""
+        node = self.root
+        for letter in word:
+            if letter not in node.children:
+                return False
+            node = node.children[letter]
+        return node.is_word
+
+
+def build_trie(words):
+    """Build a Trie from a list of words."""
+    trie = Trie()
+    for word in words:
+        trie.insert(word)
+    return trie
+
+
+def dfs(grid, trie_node, i, j, current_word, words, path, special_index):
+    """Depth-first search to generate words from the grid."""
+    if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == '#':
+        return
+
+    letter = grid[i][j]
+    if letter not in trie_node.children:
+        return
+
+    current_word += letter
+    trie_node = trie_node.children[letter]
+    path.append((i, j))
+
+    if trie_node.is_word:
+        score = calculate_score(current_word, LETTER_VALUES, special_index, path)
+        words.add((current_word, score))
+
+    grid[i][j] = '#'  # Mark as visited
+    for x, y in [(i-1, j), (i+1, j), (i, j-1), (i, j+1), (i-1, j-1), (i-1, j+1), (i+1, j-1), (i+1, j+1)]:
+        dfs(grid, trie_node, x, y, current_word, words, path, special_index)
+    grid[i][j] = letter  # Restore the letter
+    path.pop()
+
+
+def generate_words(grid, trie, special_index):
+    """Generate all valid words from the grid."""
+    words = set()
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            dfs(grid, trie.root, i, j, "", words, [], special_index)
+    return words
+
+
+def find_highest_scoring_word(grid, trie, special_index):
+    """Find the highest-scoring word in the grid."""
+    words = generate_words(grid, trie, special_index)
+    highest_score = 0
+    highest_scoring_word = ""
+
+    for word, score in words:
+        if score > highest_score:
+            highest_score = score
+            highest_scoring_word = word
+
+    return highest_scoring_word, highest_score
+
+def contains_special_character(word, special_index):
+    """Check if the word contains the special character at the specified index."""
+    return len(word) > special_index and word[special_index] == 'e'
+
+
+def calculate_score(word, letter_values, special_index, path):
+    """Calculate the score of a word based on letter values and special character."""
+    score = 0
+    for idx, letter in enumerate(word):
+        score += letter_values.get(letter, 4)
+    if (special_index // GRID_SIZE, special_index % GRID_SIZE) in path:
+        score *= 2
+    return score
+
+def get_user_input_grid(size):
+    """Get the grid input from the user."""
+    grid = []
+    print(f"Enter the letters for the {size}x{size} grid in a single line:")
+    letters = input().strip().lower()
+    if len(letters) != size * size:
+        raise ValueError("Invalid input length. Please enter the correct number of letters.")
+    for i in range(size):
+        row = list(letters[i*size:(i+1)*size])
+        grid.append(row)
+    return grid
+
+def main():
+    # Build the trie with the dictionary words
+    trie = build_trie(web2lowerset)
+
+    # Get grid and special character index from the user
+    original_grid = get_user_input_grid(GRID_SIZE)
+    special_index = int(input("Enter the special character index (0-24): "))
+
+    while True:
+        user_grid = [row.copy() for row in original_grid]  # Create a copy of the original grid
+        highest_scoring_word, highest_score = find_highest_scoring_word(user_grid, trie, special_index)
+        print(f"The highest-scoring word is '{highest_scoring_word}' with a score of {highest_score}")
+
+        # Remove the found word from the trie
+        trie.remove(highest_scoring_word)
+
+        # Ask the user if they want to continue generating words
+        user_input = input("Do you want to continue generating words? (y/n): ").strip().lower()
+        if user_input != 'y':
+            break
+
+if __name__ == "__main__":
+    main()
